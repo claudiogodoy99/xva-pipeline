35fbcb60dfb79f5509965f8b3035258be24df602
claudiogodoy99 <cavalcantigodoy@gmail.com>
2024-03-14 20:06:04 -0300

    adding event hubs for batching

---
+++ xva-file-event-controller/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using Azure.Messaging.EventHubs.Consumer;
+using System.Diagnostics;
+
+var connectionString = "<< CONNECTION STRING FOR THE EVENT HUBS NAMESPACE >>";
+var eventHubName = "<< NAME OF THE EVENT HUB >>";
+var consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+var consumer = new EventHubConsumerClient(
+    consumerGroup,
+    connectionString,
+    eventHubName);
+
+try
+{
+    while (true)
+    {
+        await Task.Delay(1);
+        await foreach (PartitionEvent partitionEvent in consumer.ReadEventsAsync())
+        {
+            string readFromPartition = partitionEvent.Partition.PartitionId;
+            //byte[] eventBodyBytes = partitionEvent.Data.EventBody.ToObject<ob>;
+        }
+    }
+}
+catch (TaskCanceledException)
+{
+    // This is expected if the cancellation token is
+    // signaled.
+}
+finally
+{
+    await consumer.CloseAsync();
+}
